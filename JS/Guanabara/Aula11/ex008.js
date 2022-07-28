e9e6ec87889e2ec1a40b21fe565b2ac16d25015a
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-07-28 08:30:28 -0300

    Initial commit

---
+++ JS/Guanabara/Aula11/ex008.js
@@ -0,0 +1,14 @@
+
+
+var vel = 60
+
+for(let i=0; i < 5; i++){
+    if(vel > 60){
+        console.log(`A velocidade é ${vel} km/h`)
+        console.log(`Você ultrapassou a velocidade permitida!`)
+    }else{
+        console.log(`Sua velocidade está dentro do limite!`)
+        console.log(`Diriga sempre usando o cinto.`)
+    }
+}
+    
---
+++ JS/Guanabara/javas.js
@@ -0,0 +1,10 @@
+function apresenta(){
+
+    var oi = `ola pessoal`;
+
+    var pessoa = window.document.getElementById('ola');
+
+    pessoa.innerHTML = `iai ${oi}`;
+
+}
+  
