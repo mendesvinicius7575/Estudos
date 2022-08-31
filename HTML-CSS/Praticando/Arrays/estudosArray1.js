4a480f4c10d1729163ed995b4d6216f87413d4a4
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-08-31 17:04:34 -0300

    Arrays

---
+++ HTML-CSS/Praticando/Arrays/estudosArray1.js
@@ -0,0 +1,25 @@
+const listaDeJogos = [
+    'Crash',
+    'Sonic',
+    'Spyro',
+    'DragonBall'
+]
+
+for(let contador = 0; contador < listaDeJogos.length; contador++){
+    console.log(`${contador+1}. ${listaDeJogos[contador]}`);
+}
+
+console.log("----------")
+
+const listaDois = [
+    {nome: "Andrea", posicao: "faxineira"},
+    {nome: "Roger", posicao: "Motorista"},
+    {nome: "Lucas", posicao: "Lutador"},
+    {nome: "Ola", posicao: "faxineira"},
+    {nome: "Sera", posicao: "Corredor"},
+
+];
+
+for(let cont in listaDois){
+    console.log("Nome: " + listaDois[cont].nome + " E Ocupação: " + listaDois[cont].posicao)
+}
